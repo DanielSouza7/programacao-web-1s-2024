2e05100f68c9a4e546c91d5077ffa3a71ba33bf7
DanielSouza7 <79504216+DanielSouza7@users.noreply.github.com>
2024-03-13 10:05:19 -0300

    Add files via upload

---
+++ atividade1/variaveisOperadores.js
@@ -0,0 +1,14 @@
+const num_A = 10;
+const num_B = 5;
+
+let soma = num_A + num_B;
+console.log(`Resultado da soma entre ${num_A} e ${num_B}: ${soma}`);
+
+let subtracao = num_A - num_B;
+console.log(`Resultado da subtração entre ${num_A} e ${num_B}: ${subtracao}`);
+
+let multiplicacao = num_A * num_B;
+console.log(`Resultado da multiplicação entre ${num_A} e ${num_B}: ${multiplicacao}`);
+
+let divisao = num_A / num_B;
+console.log(`Resultado da divisão entre ${num_A} e ${num_B}: ${divisao}`);
